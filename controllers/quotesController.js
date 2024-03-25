f16817a4a20bacc70d490211e1216fefe9eb99e8
chris.miranda <christophermiranda@pursuit.org>
2024-03-25 13:29:45 -0400

    got quotes route to show data

--- controllers/quotesController.js
+++ controllers/quotesController.js
@@ -1,7 +1,34 @@
 const express = require("express");
 const quotes = express.Router();
-const { deleteQuote } = require ('../queries/quotes')
+const { deleteQuote, getAllQuotes, getOneQuote } = require ('../queries/quotes')
 
+
+// INDEX
+quotes.get('/', async (_req, res) => {
+  try {
+    const allQuotes = await getAllQuotes();
+    res.status(200).json(allQuotes);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+// SHOW
+quotes.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const quote = await getOneQuote(id);
+    if (quote) {
+      res.json(quote);
+    } else {
+      res.status(404).json({ error: 'Quote not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+// DELETE
 quotes.delete("/:id", async (req, res) => {
   const { id } = req.params;
   const deletedQuote = await deleteQuote(id);
--- queries/quotes.js
+++ queries/quotes.js
@@ -1,5 +1,23 @@
 const db = require('../db/dbConfig.js')
 
+const getAllQuotes = async () => {
+  try {
+    const allQuotes = await db.any('SELECT * FROM quotes')
+    return allQuotes
+  } catch (error) {
+      return error
+  }
+};
+
+const getOneQuote = async (id) => {
+    try {
+      const oneQuote = await db.one('SELECT * FROM quotes WHERE id=$1', id)
+      return oneQuote
+    } catch (error) {
+      return error
+    }
+};
+
 const deleteQuote = async (id) => {
   try {
     const deletedQuote = await db.one(
@@ -12,4 +30,4 @@ const deleteQuote = async (id) => {
   }
 };
 
-module.exports = { deleteQuote }
+module.exports = { getAllQuotes, getOneQuote, deleteQuote }
